02a1eae959068d7b84a8e0e5e7bcd5d207d9b3d3
andreevilela <andrevibegoogle@hotmail.com>
2021-10-31 17:26:29 -0300

    commit inicial

---
+++ src/main/kotlin/com/iftm/mcedu/McEduApplication.kt
@@ -0,0 +1,11 @@
+package com.iftm.mcedu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class McEduApplication
+
+fun main(args: Array<String>) {
+	runApplication<McEduApplication>(*args)
+}
---
+++ src/test/kotlin/com/iftm/mcedu/McEduApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.iftm.mcedu
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class McEduApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
