8a81effa5bdff6bd0269b70d315dbc7f2e11a22d
andreevilela <andrevibegoogle@hotmail.com>
2021-11-27 22:03:53 -0300

    Correção AlunoResponse

--- src/main/kotlin/com/iftm/mcedu/aluno/AlunoResponse.kt
+++ src/main/kotlin/com/iftm/mcedu/aluno/AlunoResponse.kt
@@ -6,13 +6,10 @@ import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.validation.constraints.NotBlank
 
-class AlunoResponse(
+data class AlunoResponse(
 
-    @NotBlank
     val nome: String,
-    @NotBlank
     val email: String,
-    @NotBlank
     val foto: String
 ) {
 }
