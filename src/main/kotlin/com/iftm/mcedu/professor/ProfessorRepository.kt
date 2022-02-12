c608d049af50a5ac6d711de4f6db9631f622f967
andreevilela <andrevibegoogle@hotmail.com>
2022-02-11 22:25:03 -0300

    Swagger

--- src/main/kotlin/com/iftm/mcedu/professor/ProfessorRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.iftm.mcedu.professor
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-@Deprecated("Testando entidade Usuario")
-interface ProfessorRepository: JpaRepository<Professor, Long> {
-}
